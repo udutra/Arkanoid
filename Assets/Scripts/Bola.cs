7434f05dafed8d451e3fdc2ee9f34e6c3d6009d5
Guilherme Galarça Dutra <udutra@gmail.com>
2017-10-13 15:35:36 -0300

    Aula 06
    ok

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour {
+
+    public Vector2 velocidade;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(velocidade);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
