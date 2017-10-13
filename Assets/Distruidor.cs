8fa8cf7a3e053c64e023d8844db48f6544aba509
Guilherme Galarça Dutra <udutra@gmail.com>
2017-10-13 16:03:48 -0300

    Aula 8
    ok

---
+++ Assets/Distruidor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Distruidor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collsion)
+    {
+        GameObject collider = collsion.collider.gameObject;
+        GameObject.Destroy(collider);
+    }
+}
---
+++ Assets/Jogador.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+    public float Velocidade = 10.0f;
+    public float HorizontalAxis;
+    public new Rigidbody2D rigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HorizontalAxis = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(Velocidade * HorizontalAxis, 0);
+	}
+}
